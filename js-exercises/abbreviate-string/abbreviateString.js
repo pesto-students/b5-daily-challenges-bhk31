896d4b5544e26483e63ca8b28ffdeefe47aa8e5b
bhk31 <bhkhandelwal92@gmail.com>
2021-01-06 18:50:36 +0530

    added consition for string with single word

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,6 +1,10 @@
 function abbreviateString(str) {
   try {
     const words = str.split(' ');
+    if (words.length === 1) {
+      return str;
+    }
+
     return `${words[0]} ${words[words.length - 1].charAt(0).toUpperCase()}.`;
   } catch (e) {
     throw new Error('Please enter valid data');
--- js-exercises/abbreviate-string/abbreviateString.test.js
+++ js-exercises/abbreviate-string/abbreviateString.test.js
@@ -5,6 +5,7 @@ describe('abbreviateString', () => {
     expect(abbreviateString('Hacktober Fest')).toEqual('Hacktober F.');
     expect(abbreviateString('Leeroy Fitzgerald Jenkins')).toEqual('Leeroy J.');
     expect(abbreviateString('Some arbitrary string length here.')).toEqual('Some H.');
+    expect(abbreviateString('Bhavesh')).toEqual('Bhavesh');
   });
 
   it('throws error on invalid parameters', () => {
